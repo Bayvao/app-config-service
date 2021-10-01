2e43376d1202cbd2b7f854f1eef254d2c3ebd4ca
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2021-10-02 01:16:56 +0530

    initial commit

---
+++ src/main/java/com/micro/ex/config/service/AppConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.micro.ex.config.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micro/ex/config/service/AppConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.ex.config.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
